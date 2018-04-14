bae22ae275ab7da55ae242c8ef328d6a561b10ae
Mactherobot <magn1011@hotmail.com>
2018-04-14 23:32:41 +0200

    Added seach function, and searchview.

--- app/src/main/java/com/instafood/projectfood/activities/Main3Activity.kt
+++ app/src/main/java/com/instafood/projectfood/activities/Main3Activity.kt
@@ -1,5 +1,7 @@
 package com.instafood.projectfood.activities
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +13,7 @@ import com.google.android.flexbox.FlexboxLayout
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.instafood.projectfood.R
+import com.instafood.projectfood.models.SelectIngredient
 import com.instafood.projectfood.models.firebaseConnector
 import kotlinx.android.synthetic.main.activity_main3.*
 import android.R.attr.key
@@ -19,38 +22,78 @@ import android.R.attr.key
 
 class Main3Activity : AppCompatActivity() {
 
-    private val checkedFood = mutableListOf<String>()
+    private val checkedFood = mutableSetOf<String>()
     lateinit var collection: CollectionReference
+    private val queryList = mutableListOf<SelectIngredient>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         collection = FirebaseFirestore.getInstance().collection("ingredients")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(true)
-
         val fbConnector = firebaseConnector()
         fbConnector.getIngredientList {
-            for(ing in it) {
-                val name = ing.name
-                val flex = findViewById(R.id.flex_layout) as FlexboxLayout
-                val btn_D = ToggleButton(this)
-                btn_D.setChecked(true)
+            val sortedList = it.sortedWith(compareBy({ it.name }))
+            creatButtons(sortedList)
 
-                //btn_D.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-                btn_D.layoutParams = FlexboxLayout.LayoutParams(FlexboxLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+            val sSV = findViewById<SearchView>(R.id.searchView)
 
-                btn_D.text = name
-                btn_D.textOff = name
-                btn_D.textOn = name
-                btn_D.setOnCheckedChangeListener { _, isChecked -> isChecked(isChecked, btn_D) }
+            sSV.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            sSV.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
-                //ll_main.addView(btn_D)
-                flex.addView(btn_D)
-            }
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    for (ing in it) {
+                        if (ing.name.contains(newText))
+                            queryList.add(ing)
+                    }
+                    if (queryList.size > 0) {
+                        resetView()
+                    }
+                    val sortedList = queryList.sortedWith(compareBy({ it.name }))
+                    creatButtons(sortedList)
+
+                    queryList.removeAll(it)
+                    return false
+                }
+            })
         }
+    }
 
+    private fun creatButtons(it: List<SelectIngredient>) {
+        for (ing in it) {
+                    val name = ing.name
+                    val flex = findViewById(R.id.flex_layout) as FlexboxLayout
+                    val btn_D = ToggleButton(this)
+                    if(checkedFood.contains(name)){
+                    btn_D.setChecked(false)}
+                            else{
+                        btn_D.setChecked(true)
+                    }
 
+                    //btn_D.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    btn_D.layoutParams = FlexboxLayout.LayoutParams(FlexboxLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+                    btn_D.text = name
+                    btn_D.textOff = name
+                    btn_D.textOn = name
+                    btn_D.setOnCheckedChangeListener { _, isChecked -> isChecked(isChecked, btn_D) }
+
+                    //ll_main.addView(btn_D)
+                    flex.addView(btn_D)
+
+        }
+    }
+
+    private fun resetView(){
+        val flex = findViewById(R.id.flex_layout) as FlexboxLayout
+        flex.removeAllViews()
     }
 
     // Checks the button and adds it to the list of checked/clicked buttons.
@@ -102,3 +145,4 @@ class Main3Activity : AppCompatActivity() {
 
     }
 }
+
