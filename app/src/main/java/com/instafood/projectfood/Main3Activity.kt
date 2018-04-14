4351fcc1641bae50d67fe8200c82412cadc554c2
Christian Weis <christian.weis.8270@gmail.com>
2018-04-14 09:22:52 +0200

    Added firebase support

--- app/src/main/java/com/instafood/projectfood/Main3Activity.kt
+++ app/src/main/java/com/instafood/projectfood/Main3Activity.kt
@@ -10,12 +10,14 @@ class Main3Activity : AppCompatActivity() {
     private val checkedFood = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
 
         // Listens to the buttons and runs the isChecked method.
-        val button = findViewById(R.id.toggleButton) as ToggleButton
+        val button = findViewById<ToggleButton>(R.id.toggleButton)
         button?.setOnCheckedChangeListener { buttonView, isChecked -> isChecked(isChecked, button) }
         val button1 = findViewById<ToggleButton>(R.id.toggleButton2)
         button1?.setOnCheckedChangeListener { buttonView, isChecked -> isChecked(isChecked, button1) }
@@ -23,15 +25,11 @@ class Main3Activity : AppCompatActivity() {
         button2?.setOnCheckedChangeListener { buttonView, isChecked -> isChecked(isChecked, button2) }
     }
 
-
-
-
-
     // Checks the button and adds it to the list of checked/clicked buttons.
     private fun isChecked(isChecked: Boolean, pbutton: ToggleButton) {
         if (!isChecked) {
             checkedFood.add(pbutton.text.toString())
-            Toast.makeText(this@Main3Activity, checkedFood.size.toString(), Toast.LENGTH_SHORT).show()
+
         } else {
             checkedFood.remove(pbutton.text.toString())
             Toast.makeText(this@Main3Activity, checkedFood.size.toString(), Toast.LENGTH_SHORT).show()
