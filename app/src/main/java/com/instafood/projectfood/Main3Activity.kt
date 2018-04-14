a5cc4ba77b25c19dd2f9d1c974ca7dece8c97845
Christian Weis <christian.weis.8270@gmail.com>
2018-04-14 08:55:52 +0200

    Added some buttons and tracking for which buttons are checked

--- app/src/main/java/com/instafood/projectfood/Main3Activity.kt
+++ app/src/main/java/com/instafood/projectfood/Main3Activity.kt
@@ -1,12 +1,42 @@
 package com.instafood.projectfood
 
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class Main3Activity : AppCompatActivity() {
+
+    private val checkedFood = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+
+        // Listens to the buttons and runs the isChecked method.
+        val button = findViewById(R.id.toggleButton) as ToggleButton
+        button?.setOnCheckedChangeListener { buttonView, isChecked -> isChecked(isChecked, button) }
+        val button1 = findViewById<ToggleButton>(R.id.toggleButton2)
+        button1?.setOnCheckedChangeListener { buttonView, isChecked -> isChecked(isChecked, button1) }
+        val button2 = findViewById<ToggleButton>(R.id.toggleButton3)
+        button2?.setOnCheckedChangeListener { buttonView, isChecked -> isChecked(isChecked, button2) }
     }
+
+
+
+
+
+    // Checks the button and adds it to the list of checked/clicked buttons.
+    private fun isChecked(isChecked: Boolean, pbutton: ToggleButton) {
+        if (!isChecked) {
+            checkedFood.add(pbutton.text.toString())
+            Toast.makeText(this@Main3Activity, checkedFood.size.toString(), Toast.LENGTH_SHORT).show()
+        } else {
+            checkedFood.remove(pbutton.text.toString())
+            Toast.makeText(this@Main3Activity, checkedFood.size.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
